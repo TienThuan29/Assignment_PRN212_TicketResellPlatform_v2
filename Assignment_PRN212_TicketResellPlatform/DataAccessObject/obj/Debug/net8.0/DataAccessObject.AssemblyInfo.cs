fa3398b39b9d317eb6ac9eeef67e3cc53c60107c
Arexmarx <phannguyenmanhcuong4662@gmail.com>
2024-11-01 08:10:16 +0700

    No change

--- Assignment_PRN212_TicketResellPlatform/DataAccessObject/obj/Debug/net8.0/DataAccessObject.AssemblyInfo.cs
+++ Assignment_PRN212_TicketResellPlatform/DataAccessObject/obj/Debug/net8.0/DataAccessObject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5fe1cc1e79fc8a88c916de0746e46a18fd5de5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18785c443510e810e8ec7169f3d4cb55f17e332f")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessObject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Assignment_PRN212_TicketResellPlatform/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
+++ Assignment_PRN212_TicketResellPlatform/Repository/obj/Debug/net8.0/Repository.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Repository")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5fe1cc1e79fc8a88c916de0746e46a18fd5de5d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18785c443510e810e8ec7169f3d4cb55f17e332f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Repository")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Repository")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
